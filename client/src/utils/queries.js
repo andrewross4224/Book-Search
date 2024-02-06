9568101330016cbb82e27820f25e1f16c76459d9
andrewross4224 <andrew.ross4224@gmail.com>
2024-02-06 12:37:46 -0500

    changed query name

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_PROFILES = gql`
+export const QUERY_USER = gql`
     query singleUser($userId: ID!) {
     profile(userId: $userId) {
       _id
