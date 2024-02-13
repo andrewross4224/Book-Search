2af4e8b0cf981505aea4f2ee9cc3d21a49524f8f
Andrew Ross <andrew.ross4224@gmail.com>
2024-02-12 21:49:26 -0500

    changed server js path

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const startServer = async () => {
   }));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     })
